2a31fd57a47a6f0b04e41a6377f1017aaa651c2b
장지우 <113881972+zangzoo@users.noreply.github.com>
2024-03-28 17:35:30 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,26 +1,34 @@
-#모든 트럭이 다리를 건너려면 최소 몇초가 걸리는지 return 
-# 트럭이 최대 bridge_length 대가 올라갈 수 있음 , 다리는 weight 이하 무게만 견딤, 다리에 완전히 오르지않았으면 무게 무시
-# 정해진 순으로 건넘
-# 최단 시간 안에 다리를 건너게
+#모든 트럭이 다리를 건너려면 최소 몇소가 걸리는지
+#트럭이 최대 bridge_length대 
+#weight 이하까지의 무게, 완전히 오르지 않으면 무게 무시
 
-# 선입선출 => 큐
+#1. bridge_length 만큼 큐 길이 생성
+#2. truck_weights 돌면서 큐에 넣어주기
+#3. 큐가 weight보다 작거나 같아야 넣을 수 있음
+#4. 들어간 트럭은 bridge_length만큼 있기
 
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    b_q=deque([0]*bridge_length)
-    w_q = deque(truck_weights)
-    time=0 #걸린 시간
-    w_a=0 #총 무게
-    while len(b_q):
-        time+=1
-        w_a -=b_q[0]
-        b_q.popleft()
-        if w_q:
-            if w_a+w_q[0] <=weight:
-                w_a+=w_q[0]
-                b_q.append(w_q.popleft())
-            else:
-                b_q.append(0)
-                
-    return time   
+    
+    time = 0
+    bridge = deque([0] * bridge_length) 
+    truck_weights = deque(truck_weights) 
+    
+    currentWeight = 0 # 현재 다리 위의 무게
+    
+    while len(truck_weights) > 0:
+        time =time+1
+
+        currentWeight = currentWeight - bridge.popleft()
+
+        if currentWeight + truck_weights[0] <= weight: #현재 다리 위 무게에 올라갈 트럭의 무게 합이 한계보다 작거나 같으면
+            currentWeight = currentWeight + truck_weights[0] #현재 다리 위 무게에 더해줌 => 트럭이 올라감
+            bridge.append(truck_weights.popleft()) #다리위에 올려주기
+
+        else: 
+            bridge.append(0) 
+            
+    time += bridge_length # 다리 길이 더해주기 => 마지막 트럭이 건너는 시간?
+    
+    return time
