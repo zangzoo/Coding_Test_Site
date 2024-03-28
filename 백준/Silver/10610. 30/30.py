098288abe177721f7735d6f557a248c43884ccb2
장지우 <113881972+zangzoo@users.noreply.github.com>
2024-03-28 15:53:43 +0900

    [Silver IV] Title: 30, Time: 124 ms, Memory: 33036 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.py
@@ -0,0 +1,17 @@
+import sys
+input=sys.stdin.readline().rstrip
+
+n=list(map(str,input()))
+n_i=[]
+exist_0=False
+for i in n:
+    n_i.append(int(i))
+    if i=='0':
+        exist_0=True
+
+if exist_0==True and sum(n_i)%3==0:
+    n_i.sort(reverse=True)
+    for i in n_i:
+        print(i,end='')
+else:
+    print(-1)
