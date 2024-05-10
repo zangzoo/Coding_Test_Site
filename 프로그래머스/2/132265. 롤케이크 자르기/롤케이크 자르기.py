4bb1715e8307b95df2ec4c6d330d6f39224cf337
장지우 <113881972+zangzoo@users.noreply.github.com>
2024-05-10 15:14:22 +0900

    [level 2] Title: 롤케이크 자르기, Time: 654.18 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,30 +1,31 @@
 # 크기보다 토핑들의 종류에 관심, 조각크기와 토핑개수 신경 X, 각 조각에 동일한 가짓수의 토핑이 올라가면 공평한것!!
 # 공평하게 자르는 방법의 수를 출력해라
 
+from collections import Counter
+
 def solution(topping):
     answer = 0
-    # 철수 케이크
-    bro1 = {}
-    for t in topping:
-        if t in bro1:
-            bro1[t] += 1
+    # 변수 요소의 각 갯수를 센다
+    # 우선 내가 모든 토핑을 가지고 있다
+    me = Counter(topping)
+    # 아직 토핑이 없다
+    bro = {}
+    
+    # 순회하면서 토핑을 하나씩 동생한테도 나눠준다
+    for i in range(len(topping)):
+        if topping[i] in bro:
+            bro[topping[i]] += 1
         else:
-            bro1[t] = 1
+            bro[topping[i]] = 1
+        
+        me[topping[i]] -= 1
+        
+        if not me[topping[i]]:
+            del(me[topping[i]])
 
-    #동생 케이크
-    bro2 = {}
-    for t in topping:
-        # 둘의 딕셔너리 크기가 같다면 동일한 크기로 나눈거
-        if len(bro2) == len(bro1):
+        # 내가 가진 토핑의 갯수와 동생이 가진 갯수가 같으면
+        # 1을 더한다
+        if len(me) == len(bro):
             answer += 1
-        # 동생 케이크에 해당 토핑이 없다면 토핑 추가
-        if t not in bro2:
-            bro2[t] = 1
-        
-        # 철수 토핑에는 하나 빼주기
-        bro1[t] -= 1
-        # 만약 철수가 해당 토핑을 아예 가지고 있지 않다면 제거
-        if bro1[t] == 0:
-            del bro1[t]
-        
+
     return answer
