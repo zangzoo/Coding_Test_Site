4d9971ef83c54a4ecd1c2535fa2d1f943ed7c832
장지우 <113881972+zangzoo@users.noreply.github.com>
2024-05-16 18:30:07 +0900

    [Silver IV] Title: 선택의 기로, Time: 320 ms, Memory: 57552 KB -BaekjoonHub

--- 백준/Silver/30970. 선택의 기로/선택의 기로.py
+++ 백준/Silver/30970. 선택의 기로/선택의 기로.py
@@ -1,24 +1,20 @@
-n=int(input())
-placed=[]
-for i in range(n):
-    q,m=map(int,input().split())
-    placed.append((q,m))
+import sys
+input = sys.stdin.readline
 
-placed_1=sorted(placed, key=lambda x:(-x[0],x[1]))
-placed_2=sorted(placed, key=lambda x:(x[1],-x[0]))
-result_1=[]
-result_2=[]
-for i in placed_1[0]:
-    result_1.append(i)
-for i in placed_1[1]:
-    result_1.append(i)
-for i in placed_2[0]:
-    result_2.append(i)
-for i in placed_2[1]:
-    result_2.append(i)
-    
-for i in result_1:
-    print(i,end=' ')
-for i in result_2:
-    print(i,end=' ')
-    
+n = int(input())
+placed = []
+for i in range(n):
+    q, m = map(int, input().split())
+    placed.append((q, m))
+
+placed_1 = sorted(placed, key=lambda x: (-x[0], x[1]))
+placed_2 = sorted(placed, key=lambda x: (x[1], -x[0]))
+
+for i in range(2): 
+    for item in placed_1[i]:
+        print(item, end=' ')
+print() #줄바꿈
+
+for j in range(2):  
+    for item in placed_2[j]:
+        print(item, end=' ')
