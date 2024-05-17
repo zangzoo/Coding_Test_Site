72df461345f2d6c3a9e3d3934c9350fb04ad832e
장지우 <113881972+zangzoo@users.noreply.github.com>
2024-05-17 17:59:50 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,8 +1,5 @@
 def solution(citations):
     citations.sort(reverse=True)
-    
-    for idx , citation in enumerate(citations):
-        if idx >= citation:
-            return idx
-        
-    return len(citations)
+    # H-지수를 계산하는 과정에서 각 논문이 인용된 횟수와 그 순서 중 더 작은 값을 고려하기 위함
+    answer = max(map(min, enumerate(citations, start=1)))
+    return answer
