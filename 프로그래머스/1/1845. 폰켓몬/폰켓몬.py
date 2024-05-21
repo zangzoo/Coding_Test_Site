df0375e6caaf3b98c40ff1f613ba02b4a98b3f2c
장지우 <113881972+zangzoo@users.noreply.github.com>
2024-05-21 17:24:27 +0900

    [level 1] Title: 폰켓몬, Time: 0.20 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,9 +1,10 @@
 def solution(nums):
+    return min(len(nums)/2, len(set(nums)))
 
-    l=len(nums)
-    uni = len(set(nums))
+#     l=len(nums)
+#     uni = len(set(nums))
     
-    if uni>=l/2 :
-        return l/2
-    else:
-        return uni
+#     if uni>=l/2 :
+#         return l/2
+#     else:
+#         return uni
