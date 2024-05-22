0a77dc38a1e1c58058bb6fe2d68dc82d8002773f
장지우 <113881972+zangzoo@users.noreply.github.com>
2024-05-22 19:42:53 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,32 +1,30 @@
 def solution(genres, plays):
     answer = []
     total={}
-    # total = {장르: 재생횟수}꼴로 만들기
-    for genre,play in zip(genres,plays):
+    for genre, play in zip(genres,plays):
         if genre in total.keys():
             total[genre]+=play
         else:
             total[genre]=play
     
-    total_sorted=sorted(total.items(),key=lambda x: -x[1])
+    total_sorted=sorted(total.items(), key=lambda x: -x[1])
     
-    # (인덱스, 장르, 재생횟수) 형태로 데이터 재구성
-    genre_play_list = [(i, genre, play) for i, (genre, play) in enumerate(zip(genres, plays))]
+    # (인덱스, 장르, 재생횟수)로 데이터 재구성
+    index_genre_play = [(i,genre,play) for i,(genre,play) in enumerate(zip(genres,plays))]
     
-    # 장르별로 곡을 재생횟수 내림차순으로 정렬
-    genre_play_sorted = {}
+    # 장르별로 재생횟수 내림차순 정렬
+    index_genre_play_sorted={}
     for genre in total.keys():
-        genre_play_sorted[genre] = sorted(
-            [item for item in genre_play_list if item[1] == genre], key=lambda x: -x[2])
-
-    # 각 장르별 상위 2곡의 인덱스를 결과에 추가
-    for genre, _ in total_sorted:
-        count = 0
-        for item in genre_play_sorted[genre]:
-            if count < 2:
+        index_genre_play_sorted[genre]=sorted([item for item in index_genre_play if item[1]==genre], key=lambda x: -x[2])
+        
+    # 각 장르별 상위 두 곡의 인덱스를 결과에 추가
+    for genre,_ in total_sorted:
+        cnt=0
+        for item in index_genre_play_sorted[genre]:
+            if cnt<2:
                 answer.append(item[0])
-                count += 1
+                cnt+=1
             else:
                 break
-
-    return answer                
+    
+    return answer
