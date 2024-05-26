419e47d6513b8fd53d8d4fcc2760d6b43aca4152
장지우 <113881972+zangzoo@users.noreply.github.com>
2024-05-26 17:38:00 +0900

    [level 2] Title: 프로세스, Time: 0.13 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,12 +1,11 @@
 def solution(priorities, location):
-    answer=0
-    q= [(i,p) for i,p in enumerate(priorities)]
-        
+    answer = 0
+    queue = [(i,p) for i,p in enumerate(priorities)]
     while priorities:
-        now=q.pop(0)
-        if any(now[1]<p[1] for p in q):
-            q.append(now)
+        now=queue.pop(0)
+        if any(now[1]<q[1] for q in queue):
+            queue.append(now)
         else:
             answer+=1
-            if now[0] ==location:
+            if now[0]==location:
                 return answer
