0b3b93f30d8bc9c68b04fe3282b9157006f996d8
장지우 <113881972+zangzoo@users.noreply.github.com>
2024-05-28 18:45:22 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,34 +1,20 @@
-#모든 트럭이 다리를 건너려면 최소 몇소가 걸리는지
-#트럭이 최대 bridge_length대 
-#weight 이하까지의 무게, 완전히 오르지 않으면 무게 무시
-
-#1. bridge_length 만큼 큐 길이 생성
-#2. truck_weights 돌면서 큐에 넣어주기
-#3. 큐가 weight보다 작거나 같아야 넣을 수 있음
-#4. 들어간 트럭은 bridge_length만큼 있기
-
-from collections import deque
-
 def solution(bridge_length, weight, truck_weights):
-    
     time = 0
-    bridge = deque([0] * bridge_length) 
-    truck_weights = deque(truck_weights) 
-    
-    currentWeight = 0 # 현재 다리 위의 무게
-    
-    while len(truck_weights) > 0:
-        time =time+1
+    bridge = []  # 다리를 건너는 트럭의 정보를 저장할 리스트, (트럭 무게, 진입 시간)
+    bridge_weight = 0  # 현재 다리 위의 총 무게
 
-        currentWeight = currentWeight - bridge.popleft()
+    while truck_weights or bridge:
+        time += 1  # 시간은 항상 1씩 증가
 
-        if currentWeight + truck_weights[0] <= weight: #현재 다리 위 무게에 올라갈 트럭의 무게 합이 한계보다 작거나 같으면
-            currentWeight = currentWeight + truck_weights[0] #현재 다리 위 무게에 더해줌 => 트럭이 올라감
-            bridge.append(truck_weights.popleft()) #다리위에 올려주기
+        # 다리를 완전히 건넌 트럭을 다리에서 제거
+        if bridge and bridge[0][1] + bridge_length == time:
+            bridge_weight -= bridge.pop(0)[0]
 
-        else: 
-            bridge.append(0) 
-            
-    time += bridge_length # 다리 길이 더해주기 => 마지막 트럭이 건너는 시간?
-    
+        # 다리에 새로운 트럭을 올릴 수 있는지 확인
+        if truck_weights and bridge_weight + truck_weights[0] <= weight:
+            # 다리의 현재 무게 + 새 트럭의 무게가 최대 하중을 넘지 않으면
+            truck = truck_weights.pop(0)  # 대기 중인 트럭을 꺼내고
+            bridge.append((truck, time))  # 다리에 트럭을 올림 (트럭 무게, 현재 시간)
+            bridge_weight += truck  # 다리 위의 무게 증가
+
     return time
