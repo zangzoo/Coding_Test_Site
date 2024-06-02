e46baee82730cfc888f5c2d498cb5bbc77bae866
장지우 <113881972+zangzoo@users.noreply.github.com>
2024-06-02 15:23:47 +0900

    [level 1] Title: 최소직사각형, Time: 1.59 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,14 @@
+def solution(sizes):
+    left=0
+    right=0
+    for size in sizes:
+        if size[0]>=size[1]:
+            pass
+        else:
+            size[0],size[1]=size[1],size[0]
+        if size[0]>left:
+            left=size[0]
+        if size[1]>right:
+            right=size[1]
+    
+    return right*left
