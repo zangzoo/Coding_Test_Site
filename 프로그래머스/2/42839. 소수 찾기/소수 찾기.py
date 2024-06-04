0a0d8e2a1ba00093f090fe6d7943e7406439c6c3
장지우 <113881972+zangzoo@users.noreply.github.com>
2024-06-04 17:17:40 +0900

    [level 2] Title: 소수 찾기, Time: 0.19 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -12,19 +12,19 @@ def isDecimal(num):
     
 def solution(numbers):
     
-    li = list(numbers)
-    allNums = set()
-    for i in range(1, len(numbers)+1):
-        permutationList = permutations(li, i)
-        for perm in permutationList:
-            num = int(''.join(perm))
-            allNums.add(num)
+    li = list(numbers) #한 문자씩 list화
+    allNums = set() # 중복값들은 제거
+    for i in range(1, len(numbers)+1): #1개부터 len(numbers)개까지 permutations로 고르기
+        permutationList = permutations(li, i) #list 되어있는 numbers를 permutations 진행해서 리스트에 넣어주기
+        for perm in permutationList: #permutation되어있는 리스트 순회
+            num = int(''.join(perm)) #해당 리스트 join해서 숫자로 만들어주고 int형으로 변환
+            allNums.add(num) #미리 만들어놓은 리스트에 추가해주기
 
     
     count = 0
     for num in allNums:
         #print(num)
-        if isDecimal(num):
+        if isDecimal(num):#소수면 count+1
             count+=1
             
     return count
