c6447c97b0e5561489dd076b61e74fc18ad77354
장지우 <113881972+zangzoo@users.noreply.github.com>
2024-06-07 20:03:02 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,10 +1,18 @@
 def solution(number, k):
     answer = []
 
+    # number 순회
     for i in number:
+        # k가 0보다 크고 = 문자열을 더 제거해야하고
+        # answer이 비어있지 않고
+        # answer의 마지막 수가 i보다 작을때 
+        # => 한자리 씩 비교하며 채워넣는거
         while k > 0 and answer and answer[-1] < i:
+            # i보다 작으니까 answer에서 빼기
             answer.pop()
+            # 제거하나 했으니까
             k -= 1
+        # 큰 애들 추가해주기
         answer.append(i)
 
-    return ''.join(answer[:len(answer) - k])
+    return ''.join(answer[:len(answer) - k]) #출력해야하는 길이만큼 슬라이싱해서 합쳐서 출력
