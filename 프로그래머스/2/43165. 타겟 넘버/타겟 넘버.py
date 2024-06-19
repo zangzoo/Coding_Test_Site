4fd5bc50be32b2cb823d25e116377e24ed4b5f07
장지우 <113881972+zangzoo@users.noreply.github.com>
2024-06-19 17:49:38 +0900

    [level 2] Title: 타겟 넘버, Time: 3.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,20 +1,10 @@
-from collections import deque
-
 def solution(numbers, target):
-    answer = 0
-    q=deque()
-    n=len(numbers)
-    # 처음값 세팅
-    q.append([numbers[0],0]) 
-    q.append([-1*numbers[0],0])
-    
-    while q:
-        now,idx=q.popleft()
-        idx+=1
-        if idx<n:
-            q.append([now+numbers[idx], idx])
-            q.append([now-numbers[idx],idx])
-        else:
-            if now==target:
-                answer+=1
-    return answer
+    q = [0]
+    for n in numbers:
+        s = []
+        for _ in range(len(q)):
+            x = q.pop()
+            s.append(x + n)
+            s.append(x + n*(-1))
+        q = s.copy()
+    return q.count(target)
